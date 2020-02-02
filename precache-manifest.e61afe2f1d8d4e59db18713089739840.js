0f447082f840d4f89b15a7871f665bd827f1ed68
root <root@web>
2020-02-02 03:27:27 +0000

    Updates

---
+++ precache-manifest.e61afe2f1d8d4e59db18713089739840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe7527da3bc6cb41169eddf42f0a2ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865a8b66a3b58e463a78",
+    "url": "/static/css/main.c9aea695.chunk.css"
+  },
+  {
+    "revision": "42e4942c264587776b36",
+    "url": "/static/js/2.6ac4ec4e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6ac4ec4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865a8b66a3b58e463a78",
+    "url": "/static/js/main.39568880.chunk.js"
+  },
+  {
+    "revision": "01e45df1fcce099de363",
+    "url": "/static/js/runtime-main.83811cbb.js"
+  }
+]);
